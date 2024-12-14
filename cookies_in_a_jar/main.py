a3d3e60dd9add6f545386e80edee91fc0848730f
David Akinola <dakinola54@gmail.com>
2024-12-14 17:51:25 +0100

    feature for removing cookie from jar

--- cookies_in_a_jar/main.py
+++ cookies_in_a_jar/main.py
@@ -7,9 +7,16 @@ class Jar:
         self.capacity = capacity 
 
     def __str__(self):
+        if Jar.number_of_cookies_in_jar == 0:
+            return 'The jar is empty'
         return '🍪' * Jar.number_of_cookies_in_jar 
     
     def deposit(self, n):
         if n > self.capacity:
-            raise ValueError(f'The jar can not take more than {self.capacity} cookies')
+            raise ValueError(f"The jar can not take more than {self.capacity} cookies")
         Jar.number_of_cookies_in_jar += n
+
+    def withdraw(self, n):
+        if n > Jar.number_of_cookies_in_jar:
+            raise ValueError(f"You cannot take more than {Jar.number_of_cookies_in_jar} cookies from the jar")
+        Jar.number_of_cookies_in_jar -= n
